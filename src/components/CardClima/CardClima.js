6b00605372feecf9549913887646eba672e35001
Nahuel Moreno <nahuel23009@gmail.com>
2024-02-21 12:24:58 -0300

    Agregue mapa

--- src/components/CardClima/CardClima.js
+++ src/components/CardClima/CardClima.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react"
 import WeatherForm from "../Form/Form.js";
 
-
 function CardClima(){
     const [weather, setWeather] = useState(null)
 
@@ -12,7 +11,7 @@ function CardClima(){
         document.title = `Clima | ${weather?.location.name ?? "Argentina"}`
     },[weather])
 
-    async function loadInfo(city = "Argentina") {
+    async function loadInfo(city = "Cordoba") {
         try {
             const request = await fetch(
                 `${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_API_KEY}&q=${city}&aqi=no`
@@ -34,12 +33,14 @@ function CardClima(){
 
     return(
         <div>
-             <div>{weather?.location.country + ", " + weather?.location.name  || "No hay datos disponibles"}</div>
+             <div>{weather?.location.country + ", " + weather?.location.region + ", " + weather?.location.name  || "No hay datos disponibles"}</div>
             <img src={weather?.current.condition.icon}></img>
             <div>{"Temperatura: "+ weather?.current.temp_c + "°"|| "No hay datos disponibles"}</div>
             <div>{"Humedad: "+ weather?.current.humidity + "%"|| "No hay datos disponibles"}</div>
             <div>{"Clima: "+weather?.current.condition.text|| "No hay datos disponibles"}</div>
-            
+            <div>
+            <iframe src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13695.982562246683!2d${weather?.location.lon}5!3d${weather?.location.lat}4!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1708527676579!5m2!1ses-419!2sar`} title="mapa" width="400" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
             <WeatherForm onChangeCity={handleChangeCity}/>
         </div>
     )
